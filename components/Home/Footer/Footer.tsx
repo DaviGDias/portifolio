0cd5604ead80450704d226f470bb66df954791eb
DaviDias <davihkd@hotmail.com>
2025-03-03 21:54:00 -0300

    Update Footer.tsx

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='pt-16 pb-16 bg-slate-950 text-white flex justify-center flex-col'>
+        <div className='pt-16 pb-16 bg-slate-950 text-white flex justify-center flex-col sm:p-4'>
             <div className='mx-auto text-white flex justify-center'>
                 <p className='font-bold text-lg'>Davi <span className='text-purple-500'>Dias</span></p>
             </div>
