a5bbe469a8514da0ec8f56e55a3fa9710b6f9cee
DaviDias <davihkd@hotmail.com>
2025-03-06 21:24:55 -0300

    Update About.tsx

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -5,9 +5,9 @@ import { FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa'
 
 const About = () => {
   return (
-    <div className='w-full flex justify-center flex-col items-center text-black bg-white py-12 px-4'>
+    <div className='w-full flex justify-center flex-col items-center text-white bg-gray-800 py-12 px-4'>
       <Title>Sobre mim</Title>
-      <div id='sobre_mim' className='w-full overflow-hidden relative'>
+      <div id='sobre_mim' className='w-full overflow-hidden relative mb-8'>
 
         <div className='flex justify-center flex-col w-11/12 md:w-4/5 h-full mx-auto mt-8 lg:mt-24'>
 
@@ -15,13 +15,13 @@ const About = () => {
 
             {/* Image Content */}
             <div className='flex justify-center'>
-              <div className='w-full  sm:w-auto sm:h-64 md:w-auto md:h-80 lg:w-auto lg:h-300 overflow-hidden'>
-                <Image src='/undraw_hello_ccwj.svg' alt='Ilustração' width={500} height={500} className='object-cover w-full h-full' />
+              <div className='rounded-3xl'>
+                <Image src='/perfil-2.jpg' alt='Ilustração' width={350} height={350} className='rounded-3xl border-4 border-purple-500 ' />
               </div>
             </div>
 
             {/* Text Content */}
-            <div className='text-black text-sm sm:text-base'>
+            <div className='text-sm sm:text-base'>
               <p>
                 Olá! Sou Davi, formado em Análise e Desenvolvimento de Sistemas, com mais de 3 anos de experiência atuando como Web Designer e Analista UX. Minha paixão está em criar experiências digitais intuitivas e visuais atraentes, sempre focando na usabilidade e na melhor jornada para o usuário.
               </p>
@@ -34,16 +34,21 @@ const About = () => {
                 Seja para aprimorar um projeto existente ou criar algo do zero, estou sempre em busca de novos desafios e oportunidades para transformar ideias em experiências incríveis!
               </p>
               
-              <div className='flex gap-4 mt-6 items-center'>
-                <a href='https://www.linkedin.com/in/davigon%C3%A7alvesdias/' target='_blank' rel='noopener noreferrer' className='text-gray-700 hover:text-purple-500 transition duration-300'>
-                  <FaLinkedin size={35} />
+              <div className='flex flex-col gap-1 mt-6 text-gray-100'>
+
+              <h2>Minhas redes:</h2>
+                <div className='flex gap-4'>
+                <a href='https://www.linkedin.com/in/davigon%C3%A7alvesdias/' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300'>
+                  <FaLinkedin size={40} />
                 </a>
-                <a href='https://www.instagram.com/davidiasdev/' target='_blank' rel='noopener noreferrer' className='text-gray-700 hover:text-purple-500 transition duration-300'>
-                  <FaInstagram size={35} />
+                <a href='https://www.instagram.com/davidiasdev/' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300'>
+                  <FaInstagram size={40} />
                 </a>
-                <a href='https://wa.me/5553981247893' target='_blank' rel='noopener noreferrer' className='text-gray-700 hover:text-purple-500 transition duration-300' aria-label='Entrar em contato pelo WhatsApp'>
-                  <FaWhatsapp size={35} />
+                <a href='https://wa.me/5553981247893' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300' aria-label='Entrar em contato pelo WhatsApp'>
+                  <FaWhatsapp size={40} />
                 </a>
+                </div>
+         
               </div>
             </div>
           </div>
