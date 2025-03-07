ff2f0e7f225354e8b3f444b9496127ec1a1d567e
DaviDias <davihkd@hotmail.com>
2025-03-06 22:02:21 -0300

    atualização da responsividade da imagem

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -8,18 +8,20 @@ const About = () => {
     <div className='w-full flex justify-center flex-col items-center text-white bg-gray-800 py-12 px-4'>
       <Title>Sobre mim</Title>
       <div id='sobre_mim' className='w-full overflow-hidden relative mb-8'>
-
         <div className='flex justify-center flex-col w-11/12 md:w-4/5 h-full mx-auto mt-8 lg:mt-24'>
-
           <div className='grid grid-cols-1 lg:grid-cols-2 items-center gap-8 md:gap-12'>
-
             {/* Image Content */}
             <div className='flex justify-center'>
-              <div className='rounded-3xl'>
-                <Image src='/perfil2.jpg' alt='Imagem' width={350} height={350} className='rounded-3xl border-4 border-purple-500 ' />
+              <div className='rounded-3xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg'>
+                <Image 
+                  src='/perfil2.jpg' 
+                  alt='Imagem' 
+                  width={350} 
+                  height={350} 
+                  className='rounded-3xl border-4 border-purple-500 w-full h-auto' 
+                />
               </div>
             </div>
-
             {/* Text Content */}
             <div className='text-sm sm:text-base'>
               <p>
@@ -33,22 +35,19 @@ const About = () => {
               <p>
                 Seja para aprimorar um projeto existente ou criar algo do zero, estou sempre em busca de novos desafios e oportunidades para transformar ideias em experiências incríveis!
               </p>
-              
               <div className='flex flex-col gap-1 mt-6 text-gray-100'>
-
-              <h2>Minhas redes:</h2>
+                <h2>Minhas redes:</h2>
                 <div className='flex gap-4'>
-                <a href='https://www.linkedin.com/in/davigon%C3%A7alvesdias/' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300'>
-                  <FaLinkedin size={40} />
-                </a>
-                <a href='https://www.instagram.com/davidiasdev/' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300'>
-                  <FaInstagram size={40} />
-                </a>
-                <a href='https://wa.me/5553981247893' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300' aria-label='Entrar em contato pelo WhatsApp'>
-                  <FaWhatsapp size={40} />
-                </a>
+                  <a href='https://www.linkedin.com/in/davigon%C3%A7alvesdias/' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300'>
+                    <FaLinkedin size={40} />
+                  </a>
+                  <a href='https://www.instagram.com/davidiasdev/' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300'>
+                    <FaInstagram size={40} />
+                  </a>
+                  <a href='https://wa.me/5553981247893' target='_blank' rel='noopener noreferrer' className=' hover:text-purple-500 transition duration-300' aria-label='Entrar em contato pelo WhatsApp'>
+                    <FaWhatsapp size={40} />
+                  </a>
                 </div>
-         
               </div>
             </div>
           </div>
